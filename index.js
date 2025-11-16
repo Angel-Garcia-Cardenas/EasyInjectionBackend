cc03eb9887b59d2486cdce85d620233f899781a7
LinJingAn <jinganlin76@gmail.com>
2025-11-15 20:04:27 -0600

    add production path

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const passport = require('passport');
 const app = express();
 const http = require('http');
+const path = require('path');
 const socketService = require('./src/services/socket.service');
 
 require('./src/config/passport');
@@ -11,6 +12,13 @@ require('./src/config/database')();
 app.use(passport.initialize());
 
 require('./src/config/routes')(app);
+
+const angularDistPath = path.join(__dirname, './dist/frontend/browser');
+
+app.use(express.static(angularDistPath));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(angularDistPath, 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
